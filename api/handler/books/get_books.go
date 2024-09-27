245fe07be4493895ceddaa0b3ee6f8deb07c31a1
Mika <artemp765@gmail.com>
2024-09-27 10:31:02 +0700

    auth

--- api/handler/books/get_books.go
+++ api/handler/books/get_books.go
@@ -3,13 +3,14 @@ package books
 import (
 	"gin_test_prjct/internal/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 )
 
-func (h handler.Handler) GetBooks(c *gin.Context) {
+func GetBooks(c *gin.Context, h *gorm.DB) {
 	var books []models.Book
 
-	if result := h.DB.Find(&books); result.Error != nil {
+	if result := h.Find(&books); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
--- api/middleware/IsAuth.go
+++ api/middleware/IsAuth.go
@@ -2,13 +2,11 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
 )
 
 func CheckAuth(c *gin.Context) {
 	token, err := c.Cookie("auth")
-	log.Println(token)
 	if err != nil || token == "" {
 		c.JSON(http.StatusOK, gin.H{
 			"auth": "token not hounded",
